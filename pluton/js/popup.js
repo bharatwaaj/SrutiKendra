0072687c66dc0cfce2da5cab30e1e0deb9b2e381
bharatwaaj <bharatwaaj.1996@gmail.com>
2016-06-17 10:32:00 +0530

    Integrated Login Modal!

---
+++ pluton/js/popup.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Bharatwaaj on 16-06-2016.
+ */
+$(document).ready(function() {
+    $('#loginForm').formValidation({
+        framework: 'bootstrap',
+        excluded: ':disabled',
+        icon: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            username: {
+                validators: {
+                    notEmpty: {
+                        message: 'The username is required'
+                    }
+                }
+            },
+            password: {
+                validators: {
+                    notEmpty: {
+                        message: 'The password is required'
+                    }
+                }
+            }
+        }
+    });
+});
